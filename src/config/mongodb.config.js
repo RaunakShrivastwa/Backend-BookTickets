2492d0acdd7d2226803b63711508c807df6ff053
RaunakShrivastwa <kumarraunak4201@gmail.com>
2024-09-15 14:42:44 +0530

    commit

--- src/config/mongodb.config.js
+++ src/config/mongodb.config.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config();
-mongoose.connect(process.env.DB_URL);
+mongoose.connect(`${process.env.DB_URL}`);
 const db = mongoose.connection;
 
 db.on('error', (err) => {
